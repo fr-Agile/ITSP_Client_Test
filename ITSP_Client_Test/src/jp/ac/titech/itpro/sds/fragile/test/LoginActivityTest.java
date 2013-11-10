9c36597fcf285cd14c5ab92b94d9f3a159c6e1a2
ayemos <yuichiro1990@gmail.com>
2013-11-10 18:06:56 +0900

    LoginActivityTest

--- ITSP_Client_Test/src/jp/ac/titech/itpro/sds/fragile/test/LoginActivityTest.java
+++ ITSP_Client_Test/src/jp/ac/titech/itpro/sds/fragile/test/LoginActivityTest.java
@@ -2,10 +2,11 @@ package jp.ac.titech.itpro.sds.fragile.test;
 
 import java.util.EventListener;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import jp.ac.titech.itpro.sds.fragile.LoginActivity;
-import jp.ac.titech.itpro.sds.fragile.LoginActivity.UserLoginTask;
 import jp.ac.titech.itpro.sds.fragile.R;
+import android.os.AsyncTask;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.TouchUtils;
 import android.util.Log;
@@ -16,7 +17,8 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginActivity> {
+public class LoginActivityTest extends
+		ActivityInstrumentationTestCase2<LoginActivity> {
 	private LoginActivity mActivity;
 	private EditText email;
 	private EditText password;
@@ -24,57 +26,24 @@ public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginAct
 	private TextView loginStatusMessage;
 	private LinearLayout loginStatus;
 	CountDownLatch signal;
-	
+
 	private EventListener mListener;
 
 	public LoginActivityTest() {
 		super(LoginActivity.class);
+		signal = new CountDownLatch(1);
 	}
 
-
-	private void taskExecute(EventListener l) throws Throwable {
-		// create CountDownLatch.
-		final CountDownLatch signal = new CountDownLatch(1);
-
-		mListener = l;
-
-		runTestOnUiThread(new Runnable() {
-			public void run() {
-				try {
-					UserLoginTask task = new UserLoginTask (mActivity, new EventListener() {
-
-						public void onFinish(int result) {
-
-							if(mListener!=null){
-								mListener.onFinish(result);
-							}
-							signal.countDown(); // release
-						}
-					});
-					task.execute("nickname");
-				} catch (Exception e) {
-					Log.e("ERROR", e.getMessage());
-					fail();
-				}
-			}
-		});
-
-		try {
-			signal.await();// wait for callback
-		} catch (InterruptedException e1) {
-			fail();
-			e1.printStackTrace();
-		}
-	}
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 		mActivity = getActivity();
-		email = (EditText)mActivity.findViewById(R.id.email);
-		password = (EditText)mActivity.findViewById(R.id.password);
-		button = (Button)mActivity.findViewById(R.id.sign_in_button);
-		loginStatusMessage = (TextView)mActivity.findViewById(R.id.login_status_message);
-		loginStatus = (LinearLayout)mActivity.findViewById(R.id.login_status);
+		email = (EditText) mActivity.findViewById(R.id.email);
+		password = (EditText) mActivity.findViewById(R.id.password);
+		button = (Button) mActivity.findViewById(R.id.sign_in_button);
+		loginStatusMessage = (TextView) mActivity
+				.findViewById(R.id.login_status_message);
+		loginStatus = (LinearLayout) mActivity.findViewById(R.id.login_status);
 	}
 
 	public void testAttempotLogin_initialize() throws Exception {
@@ -84,29 +53,80 @@ public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginAct
 
 	}
 
+	public void testTestableAsyncTask() throws Throwable {
+        final CountDownLatch signal = new CountDownLatch(1);
+        
+        final AsyncTask testTask
+            = mActivity.new UserLoginTask() {
+                @Override
+                protected void onPostExecute(final Boolean result) {
+                    super.onPostExecute(result);
+                    signal.countDown();
+                }
+        };
+        
+        runTestOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+            	testTask.execute(this);
+            }
+        });
+        
+				TouchUtils.clickView(this, email);
+				// email: A@B
+				sendKeys(KeyEvent.KEYCODE_A);
+				sendKeys(KeyEvent.KEYCODE_AT);
+				sendKeys(KeyEvent.KEYCODE_B);
+				TouchUtils.clickView(this, password);
+				// password: WRONG
+				sendKeys(KeyEvent.KEYCODE_W);
+				sendKeys(KeyEvent.KEYCODE_R);
+				sendKeys(KeyEvent.KEYCODE_O);
+				sendKeys(KeyEvent.KEYCODE_N);
+				sendKeys(KeyEvent.KEYCODE_G);
+				TouchUtils.clickView(this, button);
+        signal.await(3, TimeUnit.SECONDS);
+	}
+
 	public void testAttempotLogin_emptyEmailInput() throws Exception {
 		TouchUtils.clickView(this, button);
-		assertEquals("空のemailに対するメッセージ", "入力して下さい", loginStatusMessage.getText().toString());
+		sleep(2000);
+		assertEquals("空のemailに対するメッセージ", "入力して下さい", loginStatusMessage
+				.getText().toString());
 	}
 
 	public void testAttempotLogin_invalidEmailInput() throws Exception {
 		TouchUtils.clickView(this, email);
-		// email:		A
+		// email: A
 		sendKeys(KeyEvent.KEYCODE_A);
 		TouchUtils.clickView(this, button);
-		assertEquals("無効なemailに対するメッセージ", "メールアドレスを入力して下さい", loginStatusMessage.getText().toString());
+		assertEquals("無効なemailに対するメッセージ", "メールアドレスを入力して下さい", loginStatusMessage
+				.getText().toString());
 	}
 
 	public void testAttemptLogin_wrongPassword() throws Exception {
 		TouchUtils.clickView(this, email);
-		// email:		A@B
-		sendKeys(KeyEvent.KEYCODE_A); sendKeys(KeyEvent.KEYCODE_AT); sendKeys(KeyEvent.KEYCODE_B);
+		// email: A@B
+		sendKeys(KeyEvent.KEYCODE_A);
+		sendKeys(KeyEvent.KEYCODE_AT);
+		sendKeys(KeyEvent.KEYCODE_B);
 		TouchUtils.clickView(this, password);
-		// password:	WRONG
-		sendKeys(KeyEvent.KEYCODE_W); sendKeys(KeyEvent.KEYCODE_R); sendKeys(KeyEvent.KEYCODE_O);
-		sendKeys(KeyEvent.KEYCODE_N); sendKeys(KeyEvent.KEYCODE_G);
+		// password: WRONG
+		sendKeys(KeyEvent.KEYCODE_W);
+		sendKeys(KeyEvent.KEYCODE_R);
+		sendKeys(KeyEvent.KEYCODE_O);
+		sendKeys(KeyEvent.KEYCODE_N);
+		sendKeys(KeyEvent.KEYCODE_G);
 		TouchUtils.clickView(this, button);
-		assertEquals("無効なpasswordに対するメッセージ", "パスワードが間違っています", loginStatusMessage.getText().toString());
+		assertEquals("無効なpasswordに対するメッセージ", "パスワードが間違っています",
+				loginStatusMessage.getText().toString());
 	}
 
+	public synchronized void sleep(long msec)
+    {	
+    	try
+    	{
+    		wait(msec);
+    	}catch(InterruptedException e){}
+    }
 }
